c63ac4e88d8cb9ea0604df27e27c59eb5b63ee95
viji9898 <vijitha.wijesuriya@gmail.com>
2025-11-13 19:36:21 +0530

    add-app.js remove slectedplace not used

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
-import React from "react";
 import "./App.css";
 import MapView from "./MapView";
-
-const selectedPlace = {
-  name: "BOTTLES",
-  rating: 4.3,
-  category: "Italian restaurant",
-  status: "Closed until 12:00 PM",
-  address: "67 Brushfield St, London E1 6AA, United Kingdom",
-  image:
-    "https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg?auto=compress&cs=tinysrgb&w=1200",
-};
 
 function App() {
   return (
